e96d60030fa594269a26720eeebcbeec071fc6e8
kubosho <ta2@o2p.jp>
2012-12-20 12:06:38 +0900

    molocaiからmolokaiにリネーム

--- .emacs.d/site-lisp/color-theme/themes/color-theme-molokai.el
+++ .emacs.d/site-lisp/color-theme/themes/color-theme-molokai.el
@@ -1,8 +1,4 @@
-;; Author: Adam Lloyd <lloyda2@rpi.edu>
-;;
-;; Note: Based on the molokai theme for vim by Tomas Restrepo, which
-;; is in turn based on the monokai theme for textmate by Wimer
-;; Hazenberg and a darker variant by Hamish Stuart Macpherson.
+;; Author: kubosho_ <ta2@o2p.jp>
 
 (eval-when-compile
   (require 'color-theme))
@@ -12,106 +8,68 @@
   (interactive)
   (color-theme-install
    '(color-theme-molokai
+     ;; 背景・文字・カーソル
      ((background-color . "#1B1D1E")
       (background-mode . dark)
-      (cursor-color . "#F8F8F0")
-      (foreground-color . "#F8F8F2"))
-     ((buffers-tab-face . buffers-tab)
-      (cperl-here-face . font-lock-string-face)
-      (cperl-invalid-face quote underline)
-      (cperl-pod-face . font-lock-comment-face)
-      (cperl-pod-head-face . font-lock-variable-name-face)
-      (vc-mode-face . highlight))
+      (foreground-color . "#F8F8F2")
+      (cursor-color . "#F8F8F0"))
+
      (default ((t (:background "#1B1D1E" :foreground "#F8F8F2"))))
-     (bold ((t (:bold t))))
-     (bold-italic ((t (:weight bold :slant italic))))
-     (custom-face-tag ((t (:foreground "#66D9EF" :weight bold))))
-     (custom-state ((t (:foreground "#A6E22E"))))
-     (italic ((t (:slant italic))))
+
+     ;; 選択範囲
      (region ((t (:background "#403D3D"))))
-     (underline ((t (:underline t))))
-     (css-selector ((t (:foreground "#F92672"))))
-     (css-property ((t (:foreground "#66D9EF"))))
-     (diff-added ((t (:foreground "#A6E22E" :weight bold))))
-     (diff-context ((t (:foreground "#F8F8F2"))))
-     (diff-file-header ((t (:foreground "#66D9EF" :background nil))))
-     (diff-indicator-added ((t (:foreground "#A6E22E"))))
-     (diff-indicator-removed ((t (:foreground "#F92672"))))
-     (diff-header ((t (:foreground "#F8F8F2" :background "#232526"))))
-     (diff-hunk-header ((t (:foreground "#AE81FF" :background "#232526"))))
-     (diff-removed ((t (:foreground "#F92672" :weight bold))))
-     (escape-glyph ((t (:foreground "#E6DB74"))))
-     (minibuffer-prompt ((t (:foreground "#66D9EF"))))
+
+     ;; モードライン
      (mode-line ((t (:foreground "#F8F8F2" :background "#000000"
                                  :box (:line-width 1 :color "#000000" :style released-button)))))
-     (mode-line-buffer-id ((t (:foreground nil :background "#000000" :weight semi-bold))))
-     (mode-line-inactive ((t (:foreground "#BCBCBC" :background "#000000"
-                                          :box (:line-width 1 :color "#232526")))))
-     (mode-line-mousable ((t (:foreground "#BCBCBC" :background "#000000"))))
-     (mode-line-mousable-minor-mode ((t (:foreground "#BCBCBC" :background "#000000"))))
-     (font-lock-builtin-face ((t (:foreground "#A6E22E"))))
-     (font-lock-comment-face ((t (:foreground "#465457" :slant italic))))
-     (font-lock-comment-delimiter-face ((t (:foreground "#465457" :slant italic))))
-     (font-lock-constant-face ((t (:foreground "#AE81FF"))))
-     (font-lock-doc-face ((t (:foreground "#E6DB74" :slant italic))))
-     (font-lock-function-name-face ((t (:foreground "#F92672" :slant italic))))
-     (font-lock-keyword-face ((t (:foreground "#66D9EF"))))
-     (font-lock-negation-char-face ((t (:weight bold))))
-     (font-lock-preprocessor-face ((t (:foreground "#A6E22E"))))
-     (font-lock-regexp-grouping-backslash ((t (:weight bold))))
-     (font-lock-regexp-grouping-construct ((t (:weight bold))))
-     (font-lock-string-face ((t (:foreground "#E6DB74"))))
-     (font-lock-type-face ((t (:foreground "#66D9EF"))))
-     (font-lock-variable-name-face ((t (:foreground "#F92672"))))
-     (font-lock-warning-face ((t (:foreground "#FFFFFF"
-                                              :background "#333333"))))
-     (fringe ((t (:background "#232526"))))
+     (mode-line-buffer-id ((t (:foreground nil :background nil))))
+     (mode-line-inactive ((t (:foreground "#BCBCBC" :background "#333333"
+                                          :box (:line-width 1 :color "#333333")))))
+
+     ;; ハイライト
      (highlight ((t (:foreground "#000000" :background "#C4BE89"))))
      (hl-line ((t (:background "#293739"))))
-     (icompletep-choices ((t (:foreground "#F92672"))))
-     (icompletep-determined ((t (:foreground "#A6E22E"))))
-     (icompletep-keys ((t (:foreground "#F92672"))))
-     (icompletep-nb-candidates ((t (:foreground "#AE81FF"))))
-     (isearch ((t (:foreground "#C4BE89" :background "#000000"))))
-     (isearch-fail ((t (:foreground "#FFFFFF" :background "#333333"))))
-     (lazy-highlight ((t (:foreground "#465457" :background "#000000"))))
-     (markdown-italic-face ((t (:slant italic))))
-     (markdown-bold-face ((t (:weight bold))))
-     (markdown-header-face ((t (:weight normal))))
-     (markdown-header-face-1 ((t (:foreground "#66D9EF"))))
-     (markdown-header-face-2 ((t (:foreground "#F92672"))))
-     (markdown-header-face-3 ((t (:foreground "#A6E22E"))))
-     (markdown-header-face-4 ((t (:foreground "#AE81FF"))))
-     (markdown-header-face-5 ((t (:foreground "#E6DB74"))))
-     (markdown-header-face-6 ((t (:foreground "#66D9EF"))))
-     (markdown-inline-code-face ((t (:foreground "#66D9EF"))))
-     (markdown-list-face ((t (:foreground "#A6E22E"))))
-     (markdown-blockquote-face ((t (:slant italic))))
-     (markdown-pre-face ((t (:foreground "#AE81FF"))))
-     (markdown-link-face ((t (:foreground "#66D9EF"))))
-     (markdown-reference-face ((t (:foreground "#66D9EF"))))
-     (markdown-url-face ((t (:foreground "#E6DB74"))))
-     (markdown-link-title-face ((t (:foreground "#F92672"))))
-     (markdown-comment-face ((t (:foreground "#465457"))))
-     (markdown-math-face ((t (:foreground "#AE81FF" :slant italic))))
-     (mumamo-background-chunk-major ((t (:background "#272822"))))
-     (mumamo-background-chunk-submode ((t (:background "#1B1D1E"))))
-     (outline-1 ((t (:foreground "#66D9EF"))))
-     (outline-2 ((t (:foreground "#F92672"))))
-     (outline-3 ((t (:foreground "#A6E22E"))))
-     (outline-4 ((t (:foreground "#AE81FF"))))
-     (outline-5 ((t (:foreground "#E6DB74"))))
-     (outline-6 ((t (:foreground "#66D9EF"))))
-     (outline-7 ((t (:foreground "#F92672"))))
-     (outline-8 ((t (:foreground "#A6E22E"))))
-     (secondary-selection ((t (:background "#272822"))))
-     (show-paren-match-face ((t (:foreground "#000000" :background "#FD971F"))))
-     (show-paren-mismatch-face ((t (:foreground "#960050" :background "#1E0010"))))
-     (widget-inactive-face ((t (:background "#ff0000"))))
-     (woman-addition ((t (:foreground "#AE81FF"))))
-     (woman-bold ((t (:foreground "#F92672"))))
-     (woman-italic ((t (:foreground "#A6E22E"))))
-     (woman-unknown ((t (:foreground "#66D9EF"))))
+
+     ;; 関数名
+     (font-lock-function-name-face ((t (:foreground "#FFFFFF"))))
+
+     ;; 変数名・変数の内容
+     (font-lock-variable-name-face ((t (:foreground "#FFFFFF"))))
+     (font-lock-string-face ((t (:foreground "#E6DB74"))))
+
+     ;; 特定キーワード
+     (font-lock-keyword-face ((t (:foreground "#F92672"))))
+
+     ;; Boolean
+     (font-lock-constant-face((t (:foreground "#AE81BC"))))
+
+     ;; 括弧
+     (show-paren-match-face ((t (:foreground "#1B1D1E" :background "#FD971F"))))
+     (paren-face ((t (:foreground "#A6E22A" :background nil))))
+
+     ;; コメント
+     (font-lock-comment-face ((t (:foreground "#74715D"))))
+
+     ;; CSS
+     (css-selector ((t (:foreground "#66D9EF"))))
+     (css-property ((t (:foreground "#FD971F"))))
+
+     ;; nXML-mode
+     ;; タグ名
+     (nxml-element-local-name ((t (:foreground "#F92672"))))
+     ;; 属性
+     (nxml-attribute-local-name ((t (:foreground "#66D9EF"))))
+     ;; 括弧
+     (nxml-tag-delimiter ((t (:foreground "#A6E22A"))))
+     ;; DOCTYPE宣言
+     (nxml-markup-declaration-delimiter ((t (:foreground "#74715D"))))
+
+     ;; dired
+     (dired-directory ((t (:foreground "#A6E22A"))))
+     (dired-symlink ((t (:foreground "#66D9EF"))))
+
+     ;; MMM-mode
+     (mmm-default-submode-face ((t (:foreground nil :background "#000000"))))
      )))
 
 (provide 'color-theme-molokai)
