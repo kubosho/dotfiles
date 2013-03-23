3b96e6d762fe67ed64d924da5212af7d4db7ead3
kubosho <ta2@o2p.jp>
2013-03-23 19:49:47 +0900

    拡張子によってruby-modeに紐付ける設定を追加。

--- .emacs.d/config/mode/init-ruby-mode.el
+++ .emacs.d/config/mode/init-ruby-mode.el
@@ -2,6 +2,12 @@
 ;; ruby-mode
 ;; -------------------------------------------
 
+(autoload 'ruby-mode "ruby-mode"
+  "Mode for editing ruby source files" t)
+(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+
 ;; インデントはスペース2個
 (setq ruby-indent-level 2)
 
