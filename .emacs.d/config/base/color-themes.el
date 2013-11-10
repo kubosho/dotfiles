f2d9d23451f788302ab1cba56279ee24e2706add
kubosho <ta2@o2p.jp>
2013-11-11 01:05:54 +0900

    Delete unnecessary files

--- .emacs.d/config/base/color-themes.el
+++
@@ -1,2 +0,0 @@
-(add-to-list 'custom-theme-load-path (concat "~/.emacs.d/themes"))
-(load-theme 'wombat t)
--- .emacs.d/config/mode/init-emacs-lisp-mode.el
+++
@@ -1,9 +0,0 @@
-;; -------------------------------------------
-;; emacs-lisp-mode
-;; -------------------------------------------
-
-(add-hook
- 'emacs-lisp-mode-hook
- (lambda ()
-   ;; スペースでインデントをする
-   (setq indent-tabs-mode nil)))
