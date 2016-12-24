46f2f084e754d3e6ed2de14d4e0404e3610ed718
kubosho <ta2@o2p.jp>
2016-12-25 01:51:48 +0900

    refactor(fish): update variable name

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -50,17 +50,17 @@ end
 ##################################################
 # git
 
-set fish_git_dirty_color red
-set fish_git_not_dirty_color green
+set git_dirty_color red
+set git_not_dirty_color green
 
 function parse_git_branch
   set -l branch (git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/')
   set -l git_diff (git diff)
 
   if test -n "$git_diff"
-    echo (set_color $fish_git_dirty_color)$branch(set_color normal)
+    echo (set_color $git_dirty_color)$branch(set_color normal)
   else
-    echo (set_color $fish_git_not_dirty_color)$branch(set_color normal)
+    echo (set_color $git_not_dirty_color)$branch(set_color normal)
   end
 end
 
