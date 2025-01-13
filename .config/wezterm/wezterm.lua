019e51fd260932a838146ecb1683463a2c28695d
Shota Kubota <ta2@o2p.jp>
2025-01-13 17:41:16 +0900

    feat(tmux): remove session manager configs

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,7 @@
 local wezterm = require "wezterm"
 
 local constants = require"./constants"
-local session_manager = require "./plugins/wezterm-session-manager/session-manager"
 local status = require "./status"
-local workspaces = require "./workspaces"
 local wsl = require "./wsl"
 
 local act = wezterm.action
@@ -155,23 +153,10 @@ if is_macos then
 end
 
 ------------------------------
--- State
-------------------------------
-wezterm.on("save_session", function(window) session_manager.save_state(window) end)
-wezterm.on("load_session", function(window) session_manager.load_state(window) end)
-wezterm.on("restore_session", function(window) session_manager.restore_state(window) end)
-
-------------------------------
--- Workspaces
-------------------------------
-wezterm.on("save_workspace", function(window) workspaces.save(window) end)
-wezterm.on("choose_workspace", function(window, pane) workspaces.choose(window, pane) end)
-
-------------------------------
 -- Key bindings
 ------------------------------
 config.leader = {
-  key = "t",
+  key = "w",
   mods = "CTRL",
   timeout_milliseconds = 2000
 }
@@ -198,84 +183,6 @@ config.keys = {
     },
   },
 
-  -- Workspace
-  -- refs: https://zenn.dev/sankantsu/articles/e713d52825dbbb
-  {
-    key = "W",
-    mods = "LEADER|SHIFT",
-    action = act.PromptInputLine {
-      description = wezterm.format {
-        { Attribute = { Intensity = "Bold" } },
-        { Text = "Enter name for new workspace" },
-      },
-      action = wezterm.action_callback(function(window, pane, line)
-        if line then
-          window:perform_action(
-            act.Multiple {
-              act.SwitchToWorkspace { name = line, },
-              act({ EmitEvent = "save_workspace" }),
-            },
-            pane
-          )
-        end
-      end),
-    },
-  },
-  {
-    key = "w",
-    mods = "LEADER",
-    action = act({ EmitEvent = "choose_workspace" }),
-  },
-  {
-    key = "$",
-    mods = "LEADER|SHIFT",
-    action = act.PromptInputLine {
-      description = "(wezterm) Set new title for workspace:",
-      action = wezterm.action_callback(function(_, _, line)
-        if line then
-          mux.rename_workspace(
-            mux.get_active_workspace(),
-            line
-          )
-        end
-      end),
-    },
-  },
-
-  -- Pane splitting
-  {
-    key = "-",
-    mods = "LEADER",
-    action = act.SplitVertical { domain = "CurrentPaneDomain" },
-  },
-  {
-    key = "|",
-    mods = "LEADER|SHIFT",
-    action = act.SplitHorizontal { domain = "CurrentPaneDomain" },
-  },
-
-  -- Activate pane direction
-  {
-    key = "h",
-    mods = "LEADER",
-    action = act.ActivatePaneDirection "Left",
-  },
-  {
-    key = "j",
-    mods = "LEADER",
-    action = act.ActivatePaneDirection "Down",
-  },
-  {
-    key = "k",
-    mods = "LEADER",
-    action = act.ActivatePaneDirection "Up",
-  },
-  {
-    key = "l",
-    mods = "LEADER",
-    action = act.ActivatePaneDirection "Right",
-  },
-
   -- Activate copy mode
   {
     key = "[",
@@ -288,23 +195,6 @@ config.keys = {
     key = "c",
     mods = "LEADER",
     action = act.SpawnTab "CurrentPaneDomain",
-  },
-
-  -- Session manager bindings
-  {
-    key = "S",
-    mods = "LEADER|SHIFT",
-    action = act({ EmitEvent = "save_session" })
-  },
-  {
-    key = "L",
-    mods = "LEADER|SHIFT",
-    action = act({ EmitEvent = "load_session" }),
-  },
-  {
-    key = "R",
-    mods = "LEADER|SHIFT",
-    action = act({ EmitEvent = "restore_session" }),
   },
 }
 
