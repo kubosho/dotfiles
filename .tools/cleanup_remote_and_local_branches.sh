f67516d391f18a3e5b761491e45eec2281027363
Shota Kubota <ta2@o2p.jp>
2025-03-07 11:36:03 +0900

    feat(tools): implement cleanup remote andd local branches script

---
+++ .tools/cleanup_remote_and_local_branches.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# gcl: git-cleanup-remote-and-local-branches
+#
+# Cleaning up remote and local branch is delivered as follows:
+# 1. Prune remote branches when they are deleted or merged
+# 2. Remove local branches when their remote branches are removed
+# 3. Remove local branches when a master included squash and merge commits
+
+DEFAULT_BRANCH="main"
+
+function git_prune_remote() {
+  echo "Start removing out-dated remote merged branches"
+  git fetch --prune
+  echo "Finish removing out-dated remote merged branches"
+}
+
+function git_remove_merged_local_branch() {
+  echo "Start removing out-dated local merged branches"
+  git branch --merged | egrep -v "(^\*|main)" | xargs -I % git branch -d %
+  echo "Finish removing out-dated local merged branches"
+}
+
+# When we use `Squash and merge` on GitHub,
+# `git branch --merged` cannot detect the squash-merged branches.
+# As a result, git_remove_merged_local_branch() cannot clean up
+# unused local branches. This function detects and removes local branches
+# when remote branches are squash-merged.
+#
+# There is an edge case. If you add suggested commits on GitHub,
+# the contents in local and remote are different. As a result,
+# This clean up function cannot remove local squash-merged branch.
+function git_remove_squash_merged_local_branch() {
+  echo "Start removing out-dated local squash-merged branches"
+  git checkout -q $DEFAULT_BRANCH &&
+    git for-each-ref refs/heads/ "--format=%(refname:short)" |
+    while read branch; do
+      ancestor=$(git merge-base $DEFAULT_BRANCH $branch) &&
+        [[ $(git cherry $DEFAULT_BRANCH $(git commit-tree $(git rev-parse $branch^{tree}) -p $ancestor -m _)) == "-"* ]] &&
+        git branch -D $branch
+    done
+  echo "Finish removing out-dated local squash-merged branches"
+}
+
+# Clean up remote and local branches
+function cleanup_remote_and_local_branches() {
+  git_prune_remote
+  git_remove_merged_local_branch
+  git_remove_squash_merged_local_branch
+}
+
+cleanup_remote_and_local_branches
